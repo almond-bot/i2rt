d07e074206a3ca9641e06bb04b96311d6e79f5d9
Almond Workstation <workstation@almondbot.com>
2025-10-13 19:55:00 -0500

    update can setup

--- can_setup.sh
+++ can_setup.sh
@@ -5,7 +5,13 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+# Bring down devices if they're already up
+ip link set down can_leader_l 2>/dev/null
+ip link set down can_leader_r 2>/dev/null
+ip link set down can_follower_l 2>/dev/null
+ip link set down can_follower_r 2>/dev/null
 
+# Bring up devices with configuration
 ip link set up can_leader_l type can bitrate 1000000
 ip link set up can_leader_r type can bitrate 1000000
 ip link set up can_follower_l type can bitrate 1000000
